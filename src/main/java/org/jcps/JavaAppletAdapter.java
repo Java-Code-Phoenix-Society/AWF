fc76de2f8cf4cf22ad555bd264c188a234b1d346
Justin Condello <jcondello@yahoo.com>
2024-03-09 13:22:53 +1100

    Fix some code related to Image loading.

--- src/main/java/org/jcps/JavaAppletAdapter.java
+++ src/main/java/org/jcps/JavaAppletAdapter.java
@@ -1,21 +1,26 @@
 package org.jcps;
 
+import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 /**
  * The {@code JavaAppletAdapter} interface defines methods for adapting Java applet functionality to standard applications.
  * <p>
  * This interface serves as a bridge between Java applet-based code and standard Java applications.
- * Implementations of this interface provide functionality to emulate applet-specific behaviors such as retrieving document base and applet parameters.
+ * Implementations of this interface provide functionality to emulate applet-specific behaviors such as
+ * retrieving document base and applet parameters.
  * </p>
  * <p>
- * Implementations of this interface can be used to facilitate the migration of Java applet-based code to standalone applications.
+ * Implementations of this interface can be used to facilitate the migration of
+ * Java applet-based code to standalone applications.
  * </p>
  * <p>
  * Example usage:
@@ -33,15 +38,18 @@ public interface JavaAppletAdapter {
     /**
      * Retrieves an audio clip from the specified location relative to the document base.
      * <p>
-     * This method attempts to load an audio clip from the specified location {@code s} relative to the document base {@code documentBase}.
+     * This method attempts to load an audio clip from the specified location {@code s} relative tothe document base {@code documentBase}.
      * It first checks if the document base path ends with a file separator (\ or /), and if not, appends the appropriate file separator.
-     * It then attempts to load the audio clip using {@link javax.sound.sampled.AudioSystem#getClip()} and opens it with the audio input stream obtained from the specified file path.
-     * If loading the audio clip fails due to unsupported audio file format or unavailability of audio resources, an attempt is made to load the audio clip from the specified location using a URL obtained from the class's resource and appending the specified path.
+     * It then attempts to load the audio clip using {@link javax.sound.sampled.AudioSystem#getClip()}
+     * and opens it with the audio input stream obtained from the specified file path.
+     * If loading the audio clip fails due to unsupported audio file format or unavailability of audio resources,
+     * an attempt is made to load the audio clip from the specified location using a URL obtained from
+     * the class's resource and appending the specified path.
      * If loading the audio clip still fails, an error message is printed to the console.
      * </p>
      *
      * @param documentBase a {@code String} representing the document base directory where the audio clip is located.
-     * @param fileName a {@code String} representing the file name of the audio clip relative to the document base.
+     * @param fileName     a {@code String} representing the file name of the audio clip relative to the document base.
      * @return a {@code Clip} object representing the loaded audio clip, or {@code null} if loading fails.
      */
     default Clip getAudioClip(String documentBase, String fileName) {
@@ -73,8 +81,8 @@ public interface JavaAppletAdapter {
      * <p>
      * This method is used to retrieve the document base of the application.
      * It is used to resolve relative paths to resources such as images and sound effects.
-     * The document base is the directory in which the application is stored and is used to resolve relative paths to resources.
-     * The default implementation returns the current working directory.
+     * The document base is the directory in which the application is stored and is used to resolve relative
+     * paths to resources. The default implementation returns the current working directory.
      * </p>
      * <p>
      * This method is used by the {@link JavaAppletAdapter}.
@@ -91,5 +99,46 @@ public interface JavaAppletAdapter {
             System.out.println("Security exception: " + e.getMessage());
         }
         return s;
+    }
+
+    default Object getCodeBase() {
+        Object o = null;
+        try {
+            o = this.getClass().getResource("/");
+        } catch (final Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+        return o;
+    }
+
+    default Image getImage(String o, String fileName) {
+        java.awt.Image image = null;
+        String msg = "";
+        boolean loaded = false;
+        if (o == null) {
+            o = "";
+        }
+        if (o.lastIndexOf("/") != o.length() - 1) {
+            o = o + "/";
+        }
+        // 'o' is the absolute path at this point
+        try {
+            // Try loading the image from a File
+            image = ImageIO.read(new File(o + fileName));
+            loaded = true;
+        } catch (final IOException e) {
+            msg = "Failed to load image: " + o + fileName;
+            try {
+                // Try loading the image from the packed jar resources
+                image = ImageIO.read(Objects.requireNonNull(this.getClass().getResourceAsStream("/" + fileName)));
+                loaded = true;
+            } catch (final Exception ex) {
+                msg += " & couldn't load resource: ";
+            }
+        }
+        if (!msg.isEmpty() && !loaded) {
+            System.out.println(msg);
+        }
+        return image;
     }
 }
